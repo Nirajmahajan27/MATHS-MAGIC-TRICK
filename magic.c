613cafddb4f1cc9a85d7bb5a18ca1fe40252ea72
NIRAJ MAHAJAN <96792295+Nirajmahajan27@users.noreply.github.com>
2022-02-18 14:19:25 +0530

    Add files via upload

---
+++ magic.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h> 
+#include<stdlib.h>
+int main(){
+system("cls");
+printf("______________________________________________________________________________\n");
+printf("|  ******************************  WELCOME  ******************************   |\n");
+printf("|                         I CAN GUESS THE ANSWER!                            |\n");
+printf("|____________________________________________________________________________|\n");
+printf("|                                                                            |\n");
+printf("|                   Think of a number between 1 to 10                        |\n");
+sleep(5);
+printf("|                          Add 2 in your number.                             |\n");
+sleep(5);
+printf("|                             Multiply by 2.                                 |\n");
+sleep(5);
+printf("|                               Subtract 2.                                  |\n");
+sleep(5);
+printf("|                               Divide by 2.                                 |\n");
+sleep(5);
+printf("|                        Subtract by Original number.                        |\n");
+printf("|____________________________________________________________________________|\n");
+sleep(5);
+printf("|***************************** YOUR ANSWER IS 1!*****************************|\n");
+printf("|____________________________________________________________________________|\n");
+   getch();
+}
